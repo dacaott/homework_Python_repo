438c14c27cc0b6f4e36e51f31c9da3d7654088e1
Tatiana Muromtseva <tamuromt@yandex.ru>
2025-12-17 22:43:09 +0300

    one more fix for test

--- tests/test_the_arrangement_of_queens.py
+++ tests/test_the_arrangement_of_queens.py
@@ -5,8 +5,10 @@ from src.the_arrangement_of_queens.recursive import count_queen_arrangements_rec
 
 
 def test_enumeration():
-    assert True
+    result = count_queen_arrangements_enumeration(4)
+    assert isinstance(result, int)
 
 
 def test_recursive():
-    assert True
+    result = count_queen_arrangements_recursive(4)
+    assert isinstance(result, int)
